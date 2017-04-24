57e3f8cd74cd27156e2d6e6c61b783bfb5b980e0
Kenny Hanson <Kenny Hanson>
2017-04-23 20:13:18 -0700

    Added base search-view component

---
+++ src/app/search-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-view',
+  template: `
+    
+  `,
+  styles: [`
+    
+  `],
+})
+export class SearchViewComponent {
+}
